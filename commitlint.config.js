8ed7efcc4fe82684c542293eddc96c3e0bbc42ff
James Lotery <jlotery@gmail.com>
2022-08-02 10:59:36 +0200

    fix: commitlint change

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   rules : {
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'never', 100],
+    'body-max-line-length': [0, 'always'],
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'never', 50],
+    'header-max-length': [0, 'always'],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
